c0db9a119a5ac64a421ee984511392b9db680c7c
Veeransh14 <shahveeransh@gmail.com>
2023-11-29 19:38:58 +0530

    MoneyMinder

--- flutter_application_1/lib/pages/home_page.dart
+++ flutter_application_1/lib/pages/home_page.dart
@@ -1,20 +1,96 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Widgets/drawer.dart';
-//import '../../ios/models/catalog.dart';
+import 'package:flutter_application_1/pages/login_page.dart'; // Import your login page
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final dummyList = List.generate(50, (index) => CatalogModel[0]);
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: const Color.fromARGB(215, 255, 255, 255),
       appBar: AppBar(
         title: Text('Home Page'),
         backgroundColor: Colors.deepPurple,
       ),
       drawer: MyDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Images aligned at the center with onTap gesture
+            InkWell(
+              onTap: () {
+                // Navigate to the login page when the "Savings" image is tapped
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+              child: Align(
+                alignment:
+                    Alignment.center, // Align the "Savings" image to the center
+                child: Column(
+                  children: [
+                    Ink.image(
+                      image: AssetImage(
+                        "assets/images/saving_logo.png",
+                      ),
+                      width: 150,
+                      height: 150,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(
+                        height:
+                            8), // Add space between "Savings" image and text
+                    Text(
+                      'Savings',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 16), // Add some space between the images
+            InkWell(
+              onTap: () {
+                // Handle onTap for "Finance Analysis" image if needed
+              },
+              child: Align(
+                alignment: Alignment
+                    .center, // Align the "Finance Analysis" image to the center
+                child: Column(
+                  children: [
+                    Ink.image(
+                      image: AssetImage(
+                        "assets/images/Finance_Analysis.png",
+                      ),
+                      width: 150,
+                      height: 150,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(
+                        height:
+                            8), // Add space between "Finance Analysis" image and text
+                    Text(
+                      'Finance Analysis',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 16), // Add some space between the images and text
+            // Text and other content on the right
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- flutter_application_1/lib/pages/login_page.dart
+++ flutter_application_1/lib/pages/login_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, must_be_immutable, use_key_in_widget_constructors, unused_local_variable
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/routes.dart';
@@ -9,62 +9,79 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+      color: Colors.white, // Set the background color to white
       child: SingleChildScrollView(
         child: Form(
           key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Stack(
+            alignment: Alignment.topRight,
             children: [
-              Image.asset(
-                "assets/images/hey_image.png",
-                width: 300, // Adjust the width as needed
-                height: 300, // Adjust the height as needed
-                fit: BoxFit.cover, // Use BoxFit.cover to prevent overflow
-              ),
-              Text("Welcome ",
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                  )),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Username',
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/images/hey_image.png",
+                    width: 400,
+                    height: 300,
+                    fit: BoxFit.cover,
                   ),
-                  onChanged: (value) {
-                    // String name = value;
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a username';
-                    }
-                    return null;
-                  },
-                ),
+                  Text(
+                    "Welcome ",
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                      ),
+                      onChanged: (value) {
+                        // String name = value;
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a username';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a password';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  ElevatedButton(
+                    child: Text('Login'),
+                    onPressed: () {
+                      if (_formKey.currentState?.validate() ?? false) {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      }
+                    },
+                  ),
+                ],
               ),
               Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                  ),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a password';
-                    }
-                    return null;
-                  },
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  "assets/images/MoneyMinder_Logo.png",
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.cover,
                 ),
-              ),
-              ElevatedButton(
-                child: Text('Login'),
-                onPressed: () {
-                  if (_formKey.currentState?.validate() ?? false) {
-                    Navigator.pushNamed(context, MyRoutes.homeRoute);
-                  }
-                },
               ),
             ],
           ),
