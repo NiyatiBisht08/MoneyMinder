d595bbf85cda62d25920f819bbd78c91926e005f
Veeransh14 <shahveeransh@gmail.com>
2024-01-12 10:55:14 +0530

    MoneyMinder

--- flutter_application_1/lib/pages/home_page.dart
+++ flutter_application_1/lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Widgets/drawer.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
 
+// ImageWidget class for displaying images with titles
 class ImageWidget extends StatelessWidget {
   final String imagePath;
   final String title;
@@ -12,33 +13,70 @@ class ImageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        // Handle onTap for the image if needed
-        if (title == 'Savings') {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => LoginPage()),
-          );
-        } else if (title == 'Finance Analysis') {
-          // Handle onTap for "Finance Analysis" image if needed
-        } else if (title == 'Investments') {
-          // Handle onTap for "Investment Logo" image if needed
-        }
-      },
+    return Align(
+      alignment: Alignment.topCenter,
+      child: InkWell(
+        onTap: () {
+          // Handle onTap for the image if needed
+          if (title == 'Savings') {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => LoginPage()),
+            );
+          } else if (title == 'Finance Analysis') {
+            // Handle onTap for "Finance Analysis" image if needed
+          } else if (title == 'Investments') {
+            // Handle onTap for "Investment Logo" image if needed
+          }
+        },
+        child: Column(
+          children: [
+            Ink.image(
+              image: AssetImage(imagePath),
+              width: 180, // Width same as "Investments"
+              height: 180, // Height same as "Investments"
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 4, // Decreased spacing
+            ),
+            Text(
+              title,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// SplitBillsWidget class for displaying "Split Bills" image with title
+class SplitBillsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topCenter,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Ink.image(
-            image: AssetImage(imagePath),
-            width: 150,
-            height: 150,
-            fit: BoxFit.cover,
+          InkWell(
+            onTap: () {
+              // Handle onTap for "Split Bills" image if needed
+            },
+            child: Ink.image(
+              image: AssetImage("assets/images/split_bill.png"),
+              width: 180, // Width same as "Investments"
+              height: 180, // Height same as "Investments"
+              fit: BoxFit.cover,
+            ),
           ),
           SizedBox(
-            height: 8,
+            height: 2, // Decreased spacing
           ),
           Text(
-            title,
+            'Split Bills',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
         ],
@@ -47,38 +85,7 @@ class ImageWidget extends StatelessWidget {
   }
 }
 
-class SplitBillsWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment:
-          MainAxisAlignment.center, // Center the content vertically
-      crossAxisAlignment:
-          CrossAxisAlignment.center, // Center the content horizontally
-      children: [
-        InkWell(
-          onTap: () {
-            // Handle onTap for "Split Bills" image if needed
-          },
-          child: Ink.image(
-            image: AssetImage("assets/images/split_bill.png"),
-            width: 100,
-            height: 100,
-            fit: BoxFit.cover,
-          ),
-        ),
-        SizedBox(
-          height: 8,
-        ),
-        Text(
-          'Split Bills',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-      ],
-    );
-  }
-}
-
+// HomePage class for the main home page
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -89,27 +96,48 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.deepPurple,
       ),
       drawer: MyDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            ImageWidget(
-              imagePath: "assets/images/saving_logo.png",
-              title: 'Savings',
-            ),
-            SizedBox(height: 16),
-            SplitBillsWidget(),
-            SizedBox(height: 16),
-            ImageWidget(
-              imagePath: "assets/images/Finance_Analysis.png",
-              title: 'Finance Analysis',
-            ),
-            SizedBox(height: 16),
-            ImageWidget(
-              imagePath: "assets/images/investment_logo.png",
-              title: 'Investments',
-            ),
-          ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0), // Decreased padding
+          child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.start, // Shifted towards the top
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        ImageWidget(
+                          imagePath: "assets/images/saving_logo.png",
+                          title: 'Savings',
+                        ),
+                        SizedBox(height: 4), // Decreased spacing
+                        SplitBillsWidget(),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 16), // Adjusted spacing
+                  Expanded(
+                    child: Column(
+                      children: [
+                        ImageWidget(
+                          imagePath: "assets/images/Finance_Analysis.png",
+                          title: 'Finance Analysis',
+                        ),
+                        SizedBox(height: 2), // Decreased spacing
+                        ImageWidget(
+                          imagePath: "assets/images/investment_logo.png",
+                          title: 'Investments',
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
