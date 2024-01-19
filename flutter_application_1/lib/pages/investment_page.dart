13c98d47699bfd55adfbdc7d1b60c08ccae7db7a
Veeransh14 <shahveeransh@gmail.com>
2024-01-19 11:45:38 +0530

    Delete flutter_application_1 directory

--- flutter_application_1/lib/pages/investment_page.dart
+++
@@ -1,4 +0,0 @@
-// ignore_for_file: unused_import
-import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Widgets/drawer.dart';
-import 'package:flutter_application_1/pages/investment_page.dart';
