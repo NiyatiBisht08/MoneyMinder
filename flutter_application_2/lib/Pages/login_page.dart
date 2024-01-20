f8833f1d401ba8ee6e896562a330a8c9ef9f430c
Veeransh14 <shahveeransh@gmail.com>
2024-01-20 16:30:46 +0530

    MoneyMinder

--- flutter_application_2/lib/Pages/login_page.dart
+++ flutter_application_2/lib/Pages/login_page.dart
@@ -2,23 +2,33 @@ import 'package:flutter/material.dart';
 import 'home_page.dart';
 
 class LoginPage extends StatelessWidget {
-  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
   void _signIn(BuildContext context) {
-    // You can implement your authentication logic here
-    // For simplicity, let's consider any non-empty email and password as a successful login
-    if (_emailController.text.isNotEmpty && _passwordController.text.isNotEmpty) {
+    if (_usernameController.text.isNotEmpty &&
+        _passwordController.text.isNotEmpty) {
+      // Your login logic here
       Navigator.pushReplacementNamed(context, '/home');
     } else {
-      // Handle authentication failure, e.g., show an error message
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text(
-          'Invalid email or password',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.red, // Red background for the snackbar
-      ));
+      // Show a pop-up message if either username or password is not entered
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text('Please enter both username and password.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
     }
   }
 
@@ -31,61 +41,79 @@ class LoginPage extends StatelessWidget {
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors: [
-              Color.fromARGB(255, 131, 58, 180), // Purple
-              Color.fromARGB(255, 253, 29, 29), // Red
+              Color(0xFF0099FF), // Light Blue
+              Color(0xFF4F5DFF), // Purple
             ],
           ),
         ),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Row(
             children: [
-              Image.asset(
-                'assets/instagram_logo.png', // Replace with your Instagram logo asset path
-                height: 100.0, // Adjust the height as needed
-              ),
-              SizedBox(height: 20.0), // Adjust the spacing as needed
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Your email',
-                  border: OutlineInputBorder(),
-                  filled: true,
-                  fillColor: Colors.white,
+              Expanded(
+                flex: 3, // 65% of the width
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      'Left Section',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
                 ),
-                keyboardType: TextInputType.emailAddress,
               ),
-              SizedBox(height: 16.0),
-              TextField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  hintText: 'Your password',
-                  border: OutlineInputBorder(),
-                  filled: true,
-                  fillColor: Colors.white,
-                ),
-                obscureText: true,
+              Container(
+                width: 1.0,
+                color: Colors.white,
               ),
-              SizedBox(height: 32.0),
-              ElevatedButton(
-                onPressed: () => _signIn(context),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.blue.shade800, // Darkened blue color for the button
-                ),
-                child: Text('Login'),
-              ),
-              SizedBox(height: 16.0),
-              TextButton(
-                onPressed: () {
-                  // Add navigation to sign-up page
-                  Navigator.pushNamed(context, '/signup');
-                },
-                child: Text(
-                  'Don\'t have an account? Sign Up',
-                  style: TextStyle(color: Colors.white),
+              Expanded(
+                flex: 2, // 35% of the width
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 8.0),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: TextField(
+                          controller: _usernameController,
+                          decoration: InputDecoration(
+                            labelText: 'Username',
+                            hintText: 'Enter username',
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Colors.grey[400], // Light grey
+                          ),
+                          style:
+                              TextStyle(color: Colors.black), // Dark text color
+                        ),
+                      ),
+                      SizedBox(height: 8.0),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: TextField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            hintText: 'Enter password',
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Colors.grey[400], // Light grey
+                          ),
+                          style:
+                              TextStyle(color: Colors.black), // Dark text color
+                        ),
+                      ),
+                      SizedBox(height: 16.0),
+                      ElevatedButton(
+                        onPressed: () => _signIn(context),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xFF004080), // Dark Blue
+                        ),
+                        child: Text('Login'),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
